451a85acae51a10ee372fc373b8b21c0a1c5a8b3
Fatima Alejandra Lopez Guzman <fatima.lopezgzm@uanl.edu.mx>
2024-08-24 14:26:18 -0600

    cambio de nombre de los archivos

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Actualiza la página");
+console.error("ctualiza la página");
+document.write("ctualiza la página");
+alert("ctualiza la página");
+
+document.title = ("Inteligencia de Negocios");
